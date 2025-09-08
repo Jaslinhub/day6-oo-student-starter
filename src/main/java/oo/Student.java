675293c2e0b84b1747c1b7c1ebde78655ff0d650
Jasmine <jasmine.lin@oocl.com>
2025-09-08 11:52:12 +0800

    feat:add student introduce function

---
+++ src/main/java/oo/Student.java
@@ -0,0 +1,13 @@
+package oo;
+
+public class Student extends Person{
+
+    public Student(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce()+" I am a student.";
+    }
+}
--- src/test/java/oo/step2/PersonTest.java
+++ src/test/java/oo/step2/PersonTest.java
@@ -1,10 +1,19 @@
 package oo.step2;
 
+import oo.Person;
+import oo.Student;
+import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PersonTest {
 
     // when introduce a person of student then return message with name and age
-
+@Test
+    void should_introduce_itself_when_given_a_person_of_student(){
+    Student student=new Student(1,"Tom",18);
+    assertEquals("My name is Tom. I am 18 years old. I am a student.",student.introduce());
+}
     // when introduce a person of teacher then return message with name and age
 }
