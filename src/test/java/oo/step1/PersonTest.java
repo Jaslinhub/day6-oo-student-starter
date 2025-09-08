331dbcc9666d40a0ffb014eb705b24ad9670a66b
Jasmine <jasmine.lin@oocl.com>
2025-09-08 11:34:31 +0800

    feat:add equals person id

--- src/test/java/oo/step1/PersonTest.java
+++ src/test/java/oo/step1/PersonTest.java
@@ -1,11 +1,26 @@
 package oo.step1;
 
+import oo.Person;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PersonTest {
 
 
     // when introduce a person, then return a message with name and age
-
+    @Test
+    void should_introduce_self_when_introduce_given_a_person(){
+        Person person=new Person(1,"Tom",18);
+        assertEquals("My name is Tom. I am 18 years old.",person.introduce());
+    }
     // when equals two same id person then return true
+    @Test
+    void should_return_true_when_equals_given_has_same_id(){
+        Person person1=new Person(1,"Tom",18);
+        Person person2=new Person(1,"Tom",19);
+        assertEquals(person1,person2);
+    }
 
     // when equals two different id person then return true
 
